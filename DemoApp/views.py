b1fac9fcc4f6a7e0a1f1cb5eef6e067f2ff460e1
linuxgeek140390 <39158032+linuxgeek140390@users.noreply.github.com>
2021-05-09 20:58:55 +0530

    Update views.py

--- DemoApp/views.py
+++ DemoApp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("Hello World again !!!")
+    return HttpResponse("Hello Piyush !!!")
 
 def about(request):
     return HttpResponse("This is my first django application")
